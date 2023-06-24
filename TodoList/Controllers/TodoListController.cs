72acd7ba2e9f2cf109a53ed790e9469534193386
Hossein102 <oracles20107@gmail.com>
2023-06-23 23:53:30 -0700

    Initial commit

---
+++ TodoList/Controllers/TodoListController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System;
+using System.Net;
+
+
+namespace TodoList.Controllers
+{
+   [ApiController]
+   [Route("[controller]")]
+
+   public class TodoListController : ControllerBase
+   {
+
+      [HttpGet]
+      [(HttpStatusCode.OK, "Success", typeof(string))]
+      public async Task<string> GetWebPageContent(string url)
+      {
+         // Create a new HttpClient instance
+         var httpClient = new HttpClient();
+
+         // Send an HTTP GET request to the specified URL and retrieve the response content as a string
+         var response = await httpClient.GetAsync(url);
+         var content = await response.Content.ReadAsStringAsync();
+
+         // Return the retrieved content
+         return content;
+
+      }
+   }
+}
---
+++ TodoList/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoList
+{
+   public class Todo
+   {
+      public string Title { get; set; }
+      public string Description { get; set; }
+      
+   }
+}
